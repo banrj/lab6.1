b866c9143aa5bf61ee5b1e362476e765196c3c2b
banrj <eh.ehr@bk.ru>
2022-12-25 20:21:39 +0200

    symbols

---
+++ kbk6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Богомолов Даниил Средний уровень 13 вариант
+
+Console.WriteLine("Введите текст для подсчета соотншения символов:");
+string text = Console.ReadLine();
+char[] symbols = text.ToCharArray();
+
+int totalLong = symbols.Length;
+int lowerCount = 0;
+int upCount = 0;
+for (int symbol= 0; symbol < totalLong; symbol++)
+{
+    if (char.IsLower(symbols[symbol])) lowerCount++;
+    if (char.IsUpper(symbols[symbol])) upCount++;
+}
+
+double answer = (lowerCount + upCount) / totalLong * 100;
+
+Console.WriteLine(answer + '%');
+
+Console.Write($"{totalLong} \n{lowerCount} \n {upCount}");
+
